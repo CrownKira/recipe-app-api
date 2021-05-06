826a57c89c02afdac80f744ca1393dca274ce274
CrownKira <t.karwi@yahoo.com>
2021-05-06 18:15:16 +0800

    Added manage user endpoint

--- app/core/tests/test_user_api.py
+++ app/core/tests/test_user_api.py
@@ -10,8 +10,10 @@ from rest_framework import status
 
 # config helper var and function here
 CREATE_USER_URL = reverse("user:create")
-# dont need to add auth to this api
+# don't need to add auth to this api
 TOKEN_URL = reverse("user:token")
+# me url: user/me
+ME_URL = reverse("user:me")
 
 
 # helper function
@@ -20,6 +22,7 @@ def create_user(**params):
 
 
 class PublicUserApiTests(TestCase):
+    # unauthenticated user
     """Test the users API (public)"""
 
     def setUp(self):
@@ -105,3 +108,52 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, {"email": "one", "password": ""})
         self.assertNotIn("token", res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
+        # unauthenticated request
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserAPITests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email="test@londonappdev.com", password="testpass", name="name"
+        )
+        self.client = APIClient()
+        # force authenticate this user
+        # any action performed by this client will be authenticated now
+        # the client is already logged in
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            res.data, {"name": self.user.name, "email": self.user.email}
+        )
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {"name": "new name", "password": "newpassword123"}
+
+        # make the patch request
+        res = self.client.patch(ME_URL, payload)
+
+        # update the user from the latest database, since it will be cached
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload["name"])
+        self.assertTrue(self.user.check_password(payload["password"]))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- app/user/views.py
+++ app/user/views.py
@@ -1,9 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer, AuthTokenSerializer
+
+# django-rest-framework.org/api-guide/generic-views/
 
 
 # Create your views here.
@@ -26,3 +28,27 @@ class CreateTokenView(ObtainAuthToken):
     # so that we can view this endpoint using browser
     # with the browsable api
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    # RetrieveUpdateAPIView:
+    # Used for read or update endpoints to represent a single model instance.
+    # Provides get, put and patch method handlers.
+    # this class points to a specific object
+    """Manage the authenticated user"""
+
+    serializer_class = UserSerializer
+    # authentication: will attach user to the request
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    # override get_object
+    # get the model for the logged in user
+    # Returns an object instance that should be used for detail views.
+    # Defaults to using the lookup_field parameter to filter the base queryset.
+    # May be overridden to provide more complex behavior,
+    # such as object lookups based on more than one URL kwarg.
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        # don't care about url parameter
+        return self.request.user
